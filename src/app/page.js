419e4280dda1957d63fc992c2dc0643759eb7b1e
Atik03 <atikshaharia03@gmail.com>
2026-05-21 18:29:51 +0600

    add top doctor section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import HeroBanner from "@/component/HeroBanner";
 import OurServices from "@/component/OurServices";
 import SpecializedDoctors from "@/component/SpecializedDoctors";
+import TopDoctor from "@/component/TopDoctor";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
+      <TopDoctor />
       <SpecializedDoctors />
       <OurServices />
     </>
---
+++ src/component/TopDoctor.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DoctorCard from "./DoctorCard";
+
+async function TopDoctor() {
+  const res = await fetch(`${process.env.PUBLIC_SERVER_URL}/topDoctor`);
+  const allAppointments = await res.json();
+  return (
+    <div className="py-10 px-4 md:px-none mt-15">
+      <h1 className="text-4xl text-center font-bold pb-15 ">Our Top Doctors</h1>
+      <div className="grid lg:grid-cols-4 grid-cols-1 md:grid-cols-2 gap-4">
+        {allAppointments.map((appointment) => (
+          <div key={appointment._id}>
+            <DoctorCard doctor={appointment} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TopDoctor;
